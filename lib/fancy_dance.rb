da4c0cd02b452f726c36ba167931bf1424729603
Austin Johnson <technicallyaustin@gmail.com>
2020-01-28 01:21:57 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -2,6 +2,20 @@ require_relative './class_methods_module.rb'
 require_relative './dancer.rb'
 require_relative './dance_module.rb'
 module FancyDance
+  module InstanceMethods
+    def twirl
+    end
+    def jump
+    end
+def pirouette
+end
+def tale_a_bow
+end
+
+end
+  module ClassMethods
+end
+
   include Dance
   def initialize(name)
     @name = name
